17429044f3b4178ec3ef27584004f904b41880ff
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2025-11-25 22:26:58 -0300

    criando metodo de atualizar pergunta

--- showMilhao/src/main/java/br/com/showmilhao/dao/PerguntaDao.java
+++ showMilhao/src/main/java/br/com/showmilhao/dao/PerguntaDao.java
@@ -12,6 +12,9 @@ public class PerguntaDao {
 	private Connection conexao;
 	private static final String QUERY_INSERIR= "INSERT INTO pergunta(id,nivel,enumciado,alternativa1,alternativa2,alternativa3,resposta)"
 			+ " VALUES ($next_id,?,?,?,?,?,?)";
+	private static final String QUERY_UPDATE= "UPDATE pergunta SET nivel=?,enumciado=?,"
+			+ "alternativa1=?,alternativa2=?,alternativa3=?,resposta=? WHERE id=?";
+			
 	private static final String OK = "Processo Concluido";
 	private static final int MESSAGE_TYPE = JOptionPane.INFORMATION_MESSAGE;
 	
@@ -33,6 +36,28 @@ public class PerguntaDao {
 				conexao.commit();
 				
 				JOptionPane.showMessageDialog(null, "Pergunta Adicionada com sucesso !",OK, MESSAGE_TYPE);
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+	}
+	
+	public void atualizar(Pergunta pergunta) {
+		try {
+			try(PreparedStatement statement = conexao.prepareStatement(QUERY_UPDATE)){
+				statement.setString(1,pergunta.getNivel());
+				statement.setString(2, pergunta.getEnumciado());
+				statement.setString(3, pergunta.getAlter1());
+				statement.setString(4, pergunta.getAlter2());
+				statement.setString(5, pergunta.getAlter3());
+				statement.setString(6, pergunta.getResp());
+				statement.setLong(7, pergunta.getId());
+				statement.execute();
+				conexao.commit();
+				
+				JOptionPane.showMessageDialog(null, "Modificações realizada com sucesso!",OK, MESSAGE_TYPE);
 			}
 			
 		}catch (Exception e) {
--- showMilhao/src/main/java/br/com/showmilhao/test/InsertTestPer.java
+++ showMilhao/src/main/java/br/com/showmilhao/test/InsertTestPer.java
@@ -7,14 +7,16 @@ public class InsertTestPer {
 	private static PerguntaDao dao = new PerguntaDao();
 	public static void main(String[] args) {
 		Pergunta pergunta = new Pergunta();
-		pergunta.setNivel("FACIL");
+		pergunta.setId(1L);
+		pergunta.setNivel("MEDIO");
 		pergunta.setEnumciado("Quem descobriu o Brasil?");
 		pergunta.setAlter1("JOSE");
 		pergunta.setAlter2("PELE");
 		pergunta.setAlter3("pedro alvares cabral");
 		pergunta.setResp("R pedro alvares cabral");
 		
-		dao.inserir(pergunta);
+		//dao.inserir(pergunta);
+		dao.atualizar(pergunta);
 	}
 
 }
