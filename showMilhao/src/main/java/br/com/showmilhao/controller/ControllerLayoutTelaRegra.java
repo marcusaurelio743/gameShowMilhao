c0eecf39586623440dabf2dee3d3b1ac0e9cd5f3
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2025-12-09 22:01:56 -0300

    adicionando as regras do jogo

--- showMilhao/src/main/java/br/com/showmilhao/controller/ControllerLayoutTelaRegra.java
+++ showMilhao/src/main/java/br/com/showmilhao/controller/ControllerLayoutTelaRegra.java
@@ -5,21 +5,25 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import br.com.showmilhao.util.ControllerUtil;
+import br.com.showmilhao.util.RegrasUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class ControllerLayoutTelaRegra implements Initializable {
 		private static final String  LAYOUT_TELA_INICIAL = "/view/LayoutTelaInicial.fxml";
-	private static final String  CSS = "/css/ButtonStyle.css";
-	
+		private static final String  CSS = "/css/ButtonStyle.css";
+		
+		@FXML
+		private Label labelRegras;
+		
 	@FXML
 	private void voltar(ActionEvent event) throws IOException {
 		ControllerUtil.changeLayout(getClass(), LAYOUT_TELA_INICIAL, CSS);
 		
 	}
 
-	
 	@FXML
 	private void fechar() {
 		ControllerUtil.exit();
@@ -28,7 +32,7 @@ public class ControllerLayoutTelaRegra implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		labelRegras.setText(RegrasUtil.getRegras());
 		
 	}
 
