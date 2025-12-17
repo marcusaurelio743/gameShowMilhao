37e65b8402c020d037b676542d56dfab134b274e
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2025-12-17 18:34:24 -0300

    corrigindo bug de voz do jogo

---
+++ showMilhao/src/main/java/br/com/showmilhao/controller/ControllerLayoutTelaPrincial.java
@@ -0,0 +1,13 @@
+package br.com.showmilhao.controller;
+
+import br.com.showmilhao.util.ControllerUtil;
+import javafx.fxml.FXML;
+
+public class ControllerLayoutTelaPrincial {
+	
+	@FXML
+	private void fechar() {
+		ControllerUtil.exit();
+	}
+
+}
