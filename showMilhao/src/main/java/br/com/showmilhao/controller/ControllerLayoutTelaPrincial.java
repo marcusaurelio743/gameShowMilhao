98c576bbbc31276674aaf43f9922ad4c503e2de3
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2025-12-18 13:47:15 -0300

    adicionando label de nome do jogador

--- showMilhao/src/main/java/br/com/showmilhao/controller/ControllerLayoutTelaPrincial.java
+++ showMilhao/src/main/java/br/com/showmilhao/controller/ControllerLayoutTelaPrincial.java
@@ -1,13 +1,26 @@
 package br.com.showmilhao.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import br.com.showmilhao.util.ControllerUtil;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-public class ControllerLayoutTelaPrincial {
+public class ControllerLayoutTelaPrincial implements Initializable {
+	
+	@FXML
+	private Label labelNomeJogador;
 	
 	@FXML
 	private void fechar() {
 		ControllerUtil.exit();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		labelNomeJogador.setText("xxxxxxxxxx");
 	}
 
 }
