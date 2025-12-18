9a7ed1d91f70d4b4352bfd97074da7bf31eb228e
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2025-12-18 16:29:56 -0300

    recuperando nome do jogador e adicionando na tela do jogo

--- showMilhao/src/main/java/br/com/showmilhao/controller/ControllerLayoutTelaPrincial.java
+++ showMilhao/src/main/java/br/com/showmilhao/controller/ControllerLayoutTelaPrincial.java
@@ -3,12 +3,19 @@ package br.com.showmilhao.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import br.com.showmilhao.model.Jogador;
+import br.com.showmilhao.service.JogadorService;
 import br.com.showmilhao.util.ControllerUtil;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 public class ControllerLayoutTelaPrincial implements Initializable {
+	private JogadorService jogadorService;
+	
+	public ControllerLayoutTelaPrincial() {
+		jogadorService = new JogadorService();
+	}
 	
 	@FXML
 	private Label labelNomeJogador;
@@ -20,7 +27,13 @@ public class ControllerLayoutTelaPrincial implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		labelNomeJogador.setText("xxxxxxxxxx");
+		setNome();
+	}
+	
+	private void setNome() {
+		Jogador jogador = jogadorService.listar()
+							.stream().reduce((jogador1,jogador2) -> jogador2).orElseThrow();
+		labelNomeJogador.setText(jogador.getNome());
 	}
 
 }
