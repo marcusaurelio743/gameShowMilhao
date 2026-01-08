f1caedff03981274548a97d35bb14d914cdef339
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-01-08 10:57:10 -0300

    implementar o listar ranking de jogadores

--- showMilhao/src/main/java/br/com/showmilhao/controller/ControllerLayoutTelaRanking.java
+++ showMilhao/src/main/java/br/com/showmilhao/controller/ControllerLayoutTelaRanking.java
@@ -2,22 +2,62 @@ package br.com.showmilhao.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+
 import br.com.showmilhao.model.JogadorTable;
+import br.com.showmilhao.service.JogadorService;
 import br.com.showmilhao.util.ControllerUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
 
 public class ControllerLayoutTelaRanking implements Initializable {
+	
 	private static final String  LAYOUT_TELA_INICIAL = "/view/LayoutTelaInicial.fxml";
 	private static final String  CSS = "/css/ButtonStyle.css";
+	
+	private JogadorService jogadorService;
+	
+	@FXML
+	private AnchorPane rootPane;
+	
+	@FXML
+	private Button btnLimpar;
+	
+	@FXML
+	private Button btnVoltar;
+	
+	@FXML
+	private JFXButton btnFechar;
+	
+	@FXML
+	private TableView<JogadorTable> tblRanking;
+	
+	@FXML
+	private TableColumn<JogadorTable, Integer> tblClmPosicao;
+	
+	@FXML
+	private TableColumn<JogadorTable, String> tblClmNome;
+	
+	@FXML
+	private TableColumn<JogadorTable, Integer> tblClmPontuacao;
+	
+	public ControllerLayoutTelaRanking() {
+		jogadorService = new JogadorService();
+	}
+	
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		mostrarRankingView();
 	}
 	
 	@FXML
@@ -26,7 +66,14 @@ public class ControllerLayoutTelaRanking implements Initializable {
 	}
 	
 	private List<JogadorTable> mostrarRankingView(){
-		return null;
+		List<JogadorTable> jogadores = new ArrayList<>();
+		jogadorService.listarRanking().forEach(jogador -> jogadores.add(new JogadorTable(jogador)));
+		
+		/*for (JogadorTable jogadorTable : jogadores) {
+			System.out.println(jogadorTable.getNomeTabela()+" || "+jogadorTable.getPontuacaoTable());
+		}*/
+		
+		return jogadores ;
 	}
 	
 	@FXML
